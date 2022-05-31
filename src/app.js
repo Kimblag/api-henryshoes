9ce03f8d0bb3e9fef4c78d280d6e2f0cd6ea151d
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-05-31 20:45:04 -0300

    url cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
@@ -43,8 +43,8 @@ app.use(session({
 
 
 
-app.use(cors(corsOptions), (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CORS_URL ? process.env.CORS_URL : process.env.CORS_URL_2); // update to match the domain you will make the request from
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_URL); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
