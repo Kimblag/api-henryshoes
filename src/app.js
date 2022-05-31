78dc8d6f0bfcb9e5bd515f4a7b9b508994a45069
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-05-31 18:39:14 -0300

    cambios urls

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use(session({
 
 
 app.use(cors(corsOptions), (req, res, next) => {
-  res.header("Access-Control-Allow-Origin"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
